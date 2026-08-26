7aface3ff78f82daee778bbc640514b2487c0e0f
VargaGergo-Git <detail_styrene9f@icloud.com>
2026-08-26 12:36:56 +0200

    Replace blank Settings window with MenuBarExtra UI

--- Sources/MacFanControl/App/MacFanControlApp.swift
+++ Sources/MacFanControl/App/MacFanControlApp.swift
@@ -4,71 +4,16 @@ import MacFanControlCore
 
 @main
 struct MacFanControlApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    @StateObject private var controller = FanController()
+
+    init() {
+        NSApplication.shared.setActivationPolicy(.accessory)
+    }
 
     var body: some Scene {
-        Settings {
-            EmptyView()
+        MenuBarExtra("MacFanControl", systemImage: "fanblades.fill") {
+            FanControlView(controller: controller)
         }
-    }
-}
-
-@MainActor
-final class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusItem: NSStatusItem?
-    private var popover: NSPopover?
-    private let controller = FanController()
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.accessory)
-        controller.start()
-        setupStatusItem()
-        setupTerminationHandler()
-    }
-
-    func applicationWillTerminate(_ notification: Notification) {
-        controller.releaseAllFans()
-        controller.stop()
-    }
-
-    private func setupStatusItem() {
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
-        if let button = statusItem?.button {
-            button.image = NSImage(systemSymbolName: "fanblades.fill", accessibilityDescription: "MacFanControl")
-            button.action = #selector(togglePopover(_:))
-            button.target = self
-        }
-
-        let popover = NSPopover()
-        popover.contentSize = NSSize(width: 340, height: 480)
-        popover.behavior = .transient
-        popover.animates = true
-        popover.contentViewController = NSHostingController(
-            rootView: FanControlView(controller: controller)
-        )
-
-        self.popover = popover
-    }
-
-    private func setupTerminationHandler() {
-        signal(SIGINT) { _ in
-            NSApplication.shared.terminate(nil)
-        }
-        signal(SIGTERM) { _ in
-            NSApplication.shared.terminate(nil)
-        }
-    }
-
-    @objc private func togglePopover(_ sender: AnyObject?) {
-        guard let button = statusItem?.button, let popover else { return }
-
-        if popover.isShown {
-            popover.performClose(sender)
-            return
-        }
-
-        popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
-        NSApp.activate(ignoringOtherApps: true)
+        .menuBarExtraStyle(.window)
     }
 }
