7c161543d6990cf04fcad24f3c0584dd61a9a8ea
VargaGergo-Git <detail_styrene9f@icloud.com>
2026-08-26 13:28:05 +0200

    Restore AppKit status item (MenuBarExtra freeze on macOS 27)

--- Sources/MacFanControl/App/MacFanControlApp.swift
+++ Sources/MacFanControl/App/MacFanControlApp.swift
@@ -1,22 +1,75 @@
-import SwiftUI
 import AppKit
+import SwiftUI
 import MacFanControlCore
 
 @main
-struct MacFanControlApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
-    @StateObject private var controller = FanController()
+enum MacFanControlMain {
+    static let delegate = AppDelegate()
 
-    var body: some Scene {
-        MenuBarExtra("MacFanControl", systemImage: "fanblades.fill") {
-            FanControlView(controller: controller)
-        }
-        .menuBarExtraStyle(.window)
+    static func main() {
+        let app = NSApplication.shared
+        app.setActivationPolicy(.accessory)
+        app.delegate = delegate
+        app.run()
     }
 }
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
+    private let controller = FanController()
+    private var statusItem: NSStatusItem?
+    private var popover: NSPopover?
+    private var eventMonitor: Any?
+
     func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApplication.shared.setActivationPolicy(.accessory)
+        controller.start()
+        installStatusItem()
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        controller.releaseAllFans()
+        controller.stop()
+    }
+
+    private func installStatusItem() {
+        let popover = NSPopover()
+        popover.behavior = .transient
+        popover.animates = false
+        popover.contentSize = NSSize(width: 340, height: 420)
+        popover.contentViewController = NSHostingController(
+            rootView: FanControlView(controller: controller)
+        )
+        self.popover = popover
+
+        let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "fanblades.fill", accessibilityDescription: "MacFanControl")
+            button.image?.isTemplate = true
+            button.toolTip = "MacFanControl"
+            button.target = self
+            button.action = #selector(togglePopover(_:))
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        }
+        self.statusItem = statusItem
+
+        eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown]) { [weak self] _ in
+            self?.closePopover()
+        }
+    }
+
+    @objc private func togglePopover(_ sender: Any?) {
+        guard let button = statusItem?.button, let popover else { return }
+
+        if popover.isShown {
+            closePopover()
+            return
+        }
+
+        popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+        popover.contentViewController?.view.window?.makeKey()
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
+    private func closePopover() {
+        popover?.performClose(nil)
     }
 }
